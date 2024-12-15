4a0717f0275197598d62e43292c049bde841685e
Jeff Baranski <jeff.baranski@outlook.com>
2024-12-15 13:48:23 -0500

    improve client

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { MegamillionsStatsComponent } from './megamillions-stats/megamillions-stats.component';
 import { PowerballStatsComponent } from './powerball-stats/powerball-stats.component';
+import { RngComponent } from './rng/rng.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent
+  },
+  {
+    path: 'rng',
+    component: RngComponent
   },
   {
     path: 'megamillions-stats',
---
+++ client/src/app/rng/rng.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RngComponent } from './rng.component';
+
+describe('RngComponent', () => {
+  let component: RngComponent;
+  let fixture: ComponentFixture<RngComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RngComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RngComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
